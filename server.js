e2f5d5a100b7254f9e4a08661d35a4396f8e112b
Kevin Hsu <mattandkevin1060@gmail.com>
2017-01-05 22:08:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'src')));
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-app.listen(8000, function() {
-  console.log('server up and running on port 8000');
+app.listen(port, function() {
+  console.log('server up and running on port' + port);
 })
